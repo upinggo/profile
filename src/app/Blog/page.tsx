b45624fec4ade6a71a6acde6f7885ab874225cce
Neil Shen <neil.shen@sap.com>
2026-01-29 17:30:45 +0800

    feat: wrap blog content in Navigation component for improved structure

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -1,10 +1,11 @@
+import Navigation from "@/components/common/Navigation";
 export default function Blog() {
   return (
-    <>
+    <Navigation path="/">
     do in process:
     <h1>in the further, I will visualize the markdown files for the blog</h1>
     <p>all the blog content comes from https://github.com/upinggo/shenyl.github.com/blob/gh-pages/readme.md</p>
     <p>Current working directory: {process.cwd()}</p>
-    </>
+    </Navigation>
   );
 }
